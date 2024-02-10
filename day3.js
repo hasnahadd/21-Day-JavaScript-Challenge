d1b993e24b5508b446bea0945d31493b69550afe
hasna <manelhasnahaddoud@gmail.com>
2024-02-10 16:32:29 +0100

    third day act

---
+++ day3.js
@@ -0,0 +1,13 @@
+let A =10;
+let B = 3;
+
+function OddEven(Number){
+if(Number%2 == 0){
+return `${Number} is even `
+}
+else
+return `${Number} is ODD`
+
+}
+console.log(OddEven(B))
+console.log(OddEven(A))
